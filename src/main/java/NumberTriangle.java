2816d4c7200d4a6484f70547f1f16be78807f805
baohoang288 <bao.hoang@mail.utoronto.ca>
2025-09-28 18:24:07 -0400

    Implement retrieve method for path navigation

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -89,7 +89,22 @@ public class NumberTriangle {
      */
     public int retrieve(String path) {
         // TODO implement this method
-        return -1;
+        NumberTriangle current = this;
+
+        for (int i = 0; i < path.length(); i++) {
+            char direction = path.charAt(i);
+            if (direction == 'l') {
+                current = current.left;
+            } else if (direction == 'r') {
+                current = current.right;
+            }
+
+            if (current == null) {
+                throw new IllegalArgumentException("Invalid path: node is null at position " + i);
+            }
+        }
+
+        return current.getRoot();
     }
 
     /** Read in the NumberTriangle structure from a file.
