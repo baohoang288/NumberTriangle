07a355b8e82111d7236e27da99f9f64f784ac779
baohoang288 <bao.hoang@mail.utoronto.ca>
2025-09-28 18:33:47 -0400

    Implement loadTriangle method to read triangle from file

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -109,24 +111,46 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
-
-        // will need to return the top of the NumberTriangle,
-        // so might want a variable for that.
         NumberTriangle top = null;
+        List<List<NumberTriangle>> rows = new ArrayList<>();
 
         String line = br.readLine();
+        int rowIndex = 0;
+
         while (line != null) {
+            // Split the line by whitespace to get individual numbers
+            String[] numbers = line.trim().split("\\s+");
+            List<NumberTriangle> currentRow = new ArrayList<>();
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            // Create NumberTriangle nodes for each number in this row
+            for (String numStr : numbers) {
+                int num = Integer.parseInt(numStr);
+                currentRow.add(new NumberTriangle(num));
+            }
 
-            // TODO process the line
+            rows.add(currentRow);
 
-            //read the next line
+            // If this is the first row, set it as the top
+            if (rowIndex == 0) {
+                top = currentRow.get(0);
+            } else {
+                // Connect this row to the previous row
+                List<NumberTriangle> prevRow = rows.get(rowIndex - 1);
+
+                for (int i = 0; i < prevRow.size(); i++) {
+                    NumberTriangle parent = prevRow.get(i);
+
+                    // Each parent connects to two children in the current row
+                    // Parent at position i connects to children at positions i and i+1
+                    parent.setLeft(currentRow.get(i));
+                    parent.setRight(currentRow.get(i + 1));
+                }
+            }
+
+            rowIndex++;
             line = br.readLine();
         }
+
         br.close();
         return top;
     }
